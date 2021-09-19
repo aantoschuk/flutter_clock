de38519ca87be8c83dd109c64c295cab42c54468
aantoschuk@codempire <alexander.antoschuk.dev@gmail.com>
2021-09-19 12:04:07 +0300

    last-touch

--- lib/screens/widgets/body.dart
+++ lib/screens/widgets/body.dart
@@ -1,25 +1,49 @@
 import 'package:clock_app/screens/widgets/analog_clock.dart';
-import 'package:clock_app/size_config.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 import 'digital_clock.dart';
+import '../widgets/country_card.dart';
 
 class Body extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // Box with specified width and/or height
-    return SizedBox(
-      // fill all the parent space ( double infinity )
-      width: double.infinity,
-      child: Column(children: [
-        Text(
-          "Kharkiv UA | GMT+3",
-          style: Theme.of(context).textTheme.bodyText1,
-        ),
-        DigitalClock(),
-        AnalogClock(),
-      ]),
+    return SafeArea(
+      child: SizedBox(
+        // fill all the parent space ( double infinity )
+        width: double.infinity,
+        child: Column(children: [
+          Text(
+            "Kharkiv UA | GMT+3",
+            style: Theme.of(context).textTheme.bodyText1,
+          ),
+          DigitalClock(),
+          AnalogClock(),
+          Spacer(),
+          SingleChildScrollView(
+            scrollDirection: Axis.horizontal,
+            child: Row(
+              children: [
+                CountryCard(
+                  time: "9:20",
+                  period: "PM",
+                  timeZone: "+3 HRS | EST",
+                  country: "New York, USA",
+                  iconSrc: "assets/icons/liberty.svg",
+                ),
+                CountryCard(
+                  time: "1:20",
+                  period: "AM",
+                  country: "Sydney, AU",
+                  timeZone: "+19 HRS | AEST",
+                  iconSrc: "assets/icons/sydney.svg",
+                ),
+              ],
+            ),
+          )
+        ]),
+      ),
     );
   }
 }
